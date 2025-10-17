26513af2d4ab111e7d5b6e9bcecadd65ee18c8ea
chekida <davidkoster@gmail.com>
2025-10-16 21:06:12 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,6 @@
-import time; [time.sleep(3600) for _ in iter(int, 1)]
+import http.server, socketserver, os
+
+PORT = int(os.environ.get("PORT", 8080))
+with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    print(f"Serving dummy web app on port {PORT}")
+    httpd.serve_forever()
